82b8e46d0d42a677a83b3356fdfff6a581ed8481
matias <matiasbp7@hotmail.com>
2018-06-10 17:46:48 -0300

    alta, borrar, listar y editar arreglados

--- src/php/class/config.php
+++ src/php/class/config.php
@@ -1,8 +1,8 @@
 <?php
 
 define('HOST', 'localhost');
-define('USER', 'c0280504_secac');
-define('PASSWORD', 'wimoTUwo64');
-define('DATABASE', 'c0280504_secac');
+define('USER', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'secaconstrucciones');
 
 ?> 
